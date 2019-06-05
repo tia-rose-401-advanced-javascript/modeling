bd58173a4f68c1089fd4ec2e0407870dac630d27
Tia Rose <shortyshort789@aol.com>
2019-06-05 14:08:09 -0700

    moved package out of folder

---
+++ api-server/src/models/categories.js
@@ -0,0 +1,25 @@
+'use strict';
+
+class Categories {
+
+  constructor(category) {
+    this.category = category;
+    
+
+  }
+
+  get(_id) {
+  }
+  
+  post(record) {
+  }
+
+  put(_id, record) {
+  }
+
+  delete(_id) {
+  }
+
+}
+
+module.exports = Categories;
